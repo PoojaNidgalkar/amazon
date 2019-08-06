b35153e8e2d0f2a9984689ffd4ff7db0a052e904
PoojaNidgalkar <poojanidgalkar.7@gmail.com>
2019-08-06 15:06:32 +0530

    amazon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var tinymce: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'form';
+  title = 'angulartinymc';
+  ngOnInit() { 
+    
+    tinymce.init(
+      {
+        selector: 'textarea',  // change this value according to your HTML
+        menubar: false,
+        toolbar: 'bullist, numlist',
+        plugins: 'advlist',
+        advlist_bullet_styles: 'square',
+        advlist_number_styles: 'lower-alpha,lower-roman,upper-alpha,upper-roman'
+      });
+    }
+
+  
 }
+
+
+
+
+
